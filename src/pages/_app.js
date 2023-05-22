f30e25710749b91da3e14cdf621503afdb638d5e
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-05-22 15:51:28 -0400

    Import connectdb to connect the app .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
+import connectDB from '../../config/db'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  connectDB()
   return <Component {...pageProps} />
 }
