41c360781cfaba019fc332c935e552089f8bbe02
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-05-30 21:24:47 -0400

    Remove connectDB function.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,5 @@
-import connectDB from '../../config/db'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  connectDB()
   return <Component {...pageProps} />
 }
