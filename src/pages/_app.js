7b2da974607309169b119957048c119d5dcd875e
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-07 23:14:52 -0400

    Add session component.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import { SessionProvider } from 'next-auth/react'
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
