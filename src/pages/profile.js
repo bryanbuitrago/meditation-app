e74367ac8c08b57efe711cc6b680890a40420c42
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-07 23:13:46 -0400

    Modify component.

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,9 +1,34 @@
+import { useSession } from "next-auth/react";
+import UserProfile from "@/components/UserProfile";
+import { getServerSession } from "next-auth";
+
+
+
+
 function ProfilePage() {
-    return (
-        <div>
-            <h1>User Profile Page</h1>
-        </div>
-    );
+    return <UserProfile />
+}
+
+export async function getServerSideProps({ req, res }) {
+    const session = await getServerSession(req, res)
+
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/signup',
+                permanent: false,
+            },
+        }
+    }
+
+
+    return {
+        props: {
+            session: JSON.parse(JSON.stringify(session)),
+        },
+    }
 }
 
 export default ProfilePage;
+
